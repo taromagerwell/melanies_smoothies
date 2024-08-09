0dd874fc8229fc64ad434dbc881ca37bd05943cf
taromagerwell <55579035+taromagerwell@users.noreply.github.com>
2024-08-09 10:39:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,8 @@ if ingredients_list:
             st.subheader(fruit_chosen + " Nutrition Information")
             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
             fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-        else
+            
+        else:
             st.write(fruit_chosen + ' cannot be found in the database.')
 
     #st.write(ingredients_string)
